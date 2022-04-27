c9f978a7f870489e703f246803bf952b1bdfff90
HrOokami <dev@hrookami.icu>
2022-04-27 18:45:50 +0300

    Fix

--- libadb/src/libadb/api/guild/event/guild-create.cpp
+++ libadb/src/libadb/api/guild/event/guild-create.cpp
@@ -1,6 +1,7 @@
 #include <libadb/api/guild/event/guild-create.hpp>
 #include <nlohmann/json.hpp>
 #include <libadb/types/helpers/json-optional.hpp>
+#include <libadb/types/helpers/json-chrono.hpp>
 using namespace adb::api;
 using namespace adb::types;
 
@@ -21,5 +22,11 @@ void adb::api::to_json(nlohmann::json& j, const GuildCreate& guild)
 void adb::api::from_json(const nlohmann::json& j, GuildCreate& guild)
 {
     from_json(j, *static_cast<Guild*>(&guild));
-    
+    map_from_json(j, "joined_at", guild.joinedAt);
+    map_from_json(j, "large", guild.large);
+    map_from_json(j, "unavailable", guild.unavailable);
+    map_from_json(j, "voice_states", guild.voiceStates);
+    map_from_json(j, "members", guild.members);
+    map_from_json(j, "channels", guild.channels);
+    map_from_json(j, "threads", guild.threads);
 }
