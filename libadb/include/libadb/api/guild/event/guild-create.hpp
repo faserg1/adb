23da9ded0fbd820b56923c4c78db26400346bb66
HrOokami <dev@hrookami.icu>
2022-04-27 18:41:40 +0300

    Guild create update

--- libadb/include/libadb/api/guild/event/guild-create.hpp
+++ libadb/include/libadb/api/guild/event/guild-create.hpp
@@ -2,12 +2,16 @@
 
 #include <libadb/libadb.hpp>
 #include <libadb/api/guild/data/guild.hpp>
+#include <libadb/api/guild/data/guild-member.hpp>
+#include <libadb/api/channel/data/channel.hpp>
 #include <nlohmann/json_fwd.hpp>
 #include <libadb/types/snowflake.hpp>
 #include <libadb/types/nullable.hpp>
 #include <libadb/types/time.hpp>
+#include <libadb/api/media/data/voice-state.hpp>
 #include <optional>
 #include <string>
+#include <vector>
 
 namespace adb::api
 {
@@ -17,10 +21,23 @@ namespace adb::api
      */
     struct GuildCreate : Guild
     {
+        /// when this guild was joined at
         std::optional<adb::types::TimePoint> joinedAt;
+        /// true if this is considered a large guild
         std::optional<bool> large;
+        /// true if this guild is unavailable due to an outage
         std::optional<bool> unavailable;
+        /// total number of members in this guild
         std::optional<int> memberCount;
+        /// states of members currently in voice channels; lacks the `guild_id` key
+        std::optional<std::vector<VoiceState>> voiceStates;
+        /// users in the guild
+        std::optional<std::vector<GuildMember>> members;
+        /// channels in the guild
+        std::optional<std::vector<Channel>> channels;
+        /// all active threads in the guild that current user has permission to view
+        std::optional<std::vector<Channel>> threads;
+        // TODO: presences, etc...
     };
 
     LIBADB_API void to_json(nlohmann::json& j, const GuildCreate& guild);
--- libadb/src/libadb/api/guild/event/guild-create.cpp
+++ libadb/src/libadb/api/guild/event/guild-create.cpp
@@ -1,13 +1,25 @@
 #include <libadb/api/guild/event/guild-create.hpp>
+#include <nlohmann/json.hpp>
+#include <libadb/types/helpers/json-optional.hpp>
 using namespace adb::api;
 using namespace adb::types;
+
+// TODO: 
 
 void adb::api::to_json(nlohmann::json& j, const GuildCreate& guild)
 {
     to_json(j, *static_cast<const Guild*>(&guild));
+    map_to_json(j, "joined_at", guild.joinedAt);
+    map_to_json(j, "large", guild.large);
+    map_to_json(j, "unavailable", guild.unavailable);
+    map_to_json(j, "voice_states", guild.voiceStates);
+    map_to_json(j, "members", guild.members);
+    map_to_json(j, "channels", guild.channels);
+    map_to_json(j, "threads", guild.threads);
 }
 
 void adb::api::from_json(const nlohmann::json& j, GuildCreate& guild)
 {
     from_json(j, *static_cast<Guild*>(&guild));
+    
 }
