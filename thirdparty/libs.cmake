726fa75d052e9c536f23fe03e7814643c5f2f704
HrOokami <dev@hrookami.icu>
2022-05-11 18:32:03 +0300

    Libs edit

--- thirdparty/libs.cmake
+++ thirdparty/libs.cmake
@@ -1,5 +1,7 @@
 find_package(Threads REQUIRED)
-
+if (!WIN32)
+    find_package(loguru CONFIG REQUIRED)
+endif(!WIN32)
 find_package(nlohmann_json CONFIG REQUIRED)
 find_package(fmt CONFIG REQUIRED)
 find_package(cpr CONFIG REQUIRED)
@@ -10,13 +12,19 @@ find_package(date CONFIG REQUIRED)
 find_package(OpenSSL REQUIRED)
 find_package(Boost REQUIRED COMPONENTS random system)
 
+find_path(BEXT_SML_INCLUDE_DIRS "boost/sml.hpp")
 find_path(LOGURU_INCLUDE_DIRS "loguru/loguru.cpp")
 
 find_package(unofficial-sodium CONFIG REQUIRED)
 find_package(Opus CONFIG REQUIRED)
 
 set(THIRDPARTY_SOURCE
-    ${LOGURU_INCLUDE_DIRS}/loguru/loguru.cpp
 )
+
+if (WIN32)
+    list(APPEND THIRDPARTY_SOURCE
+        ${LOGURU_INCLUDE_DIRS}/loguru/loguru.cpp
+    )
+endif (WIN32)
 
 add_library(thirdparty_libs STATIC ${THIRDPARTY_SOURCE})
