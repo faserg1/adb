d837736b197d65ba9f52e1174a0443880e5eefce
HrOokami <dev@hrookami.icu>
2022-05-11 20:12:12 +0300

    I hate this port lib

--- libadb/src/libadb/api/error/check-endpoint-error.cpp
+++ libadb/src/libadb/api/error/check-endpoint-error.cpp
@@ -1,12 +1,15 @@
 #include <libadb/api/error/check-endpoint-error.hpp>
 #include <libadb/api/error/endpoint-error.hpp>
 #include <nlohmann/json.hpp>
+#include <loguru/loguru.hpp>
 
 bool adb::api::checkEndpointError(const nlohmann::json &errorMsg)
 {
     if (errorMsg.contains("code") && errorMsg.contains("message"))
     {
-        throw adb::api::EndpointError(errorMsg.get<adb::api::Error>());
+        const auto error = errorMsg.get<adb::api::Error>();
+        LOG_F(ERROR, error.message.c_str());
+        throw adb::api::EndpointError(error);
         return true;
     }
     return false;
